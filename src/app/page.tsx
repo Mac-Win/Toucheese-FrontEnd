3a9e77aae4ce1742e0c558a3b6ac9cb3603340c6
NAM9184 <ruelline91@naver.com>
2024-11-23 02:20:29 +0900

    💄UI수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,20 @@ import Link from "next/link";
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center w-full h-full">
+    <div className="flex flex-col items-center w-full min-h-screen bg-white">
+      {/* Header */}
       <Header />
 
+      {/* Search Bar */}
       <SearchBar />
 
       {/* 콘텐츠 */}
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 p-4 w-full min-h-[50vh]">
+      <div className="grid grid-cols-2 gap-4 p-4 w-full">
         {concepts.map((concept) => (
           <Link
             href={`/studios?conceptId=${concept.id}`}
             key={concept.id}
-            className="relative rounded-lg overflow-hidden shadow-md group"
+            className="relative rounded-lg overflow-hidden shadow-md group aspect-[3/4]"
             aria-label={concept.title}
           >
             {/* Image */}
