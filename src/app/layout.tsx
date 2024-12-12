211e7335d16b12c8718037e8fcf0e1a5b02bc99f
ruelline <ruelline91@naver.com>
2024-12-12 15:44:35 +0900

    viewport 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,8 @@ export const metadata: Metadata = {
     icon: "/icon.ico",
     shortcut: "/icon32.png",
   },
+  viewport:
+    "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
 };
 
 export default function RootLayout({
