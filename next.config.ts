11a9397edfc929179bc30c0709f4188e10305ca3
ruelline <ruelline91@naver.com>
2024-12-12 13:58:14 +0900

    ♻️ 이미지 최적화 - 이미지경로 수정

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "i.toucheese-macwin.store",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
   webpack(config, { isServer }) {
