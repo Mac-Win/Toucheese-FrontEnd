a1df9de41957aaa6c713b1b9956f41f60451d35a
ruelline <ruelline91@naver.com>
2024-12-18 14:10:40 +0900

    ✨ 컴펌모달 연결결

--- src/features/cart/components/ConfirmModal.tsx
+++ src/features/cart/components/ConfirmModal.tsx
@@ -1,5 +1,5 @@
 interface ConfirmModalProps {
-  title: string;
+  title?: string;
   message: string;
   onConfirm: () => void;
   onCancel: () => void;
@@ -13,18 +13,18 @@ const ConfirmModal: React.FC<ConfirmModalProps> = ({
 }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 ">
-      <div className="bg-white p-6 rounded shadow-lg max-w-custom w-full">
+      <div className="bg-white p-6 rounded-lg shadow-lg max-w-custom w-full">
         <h2 className="text-lg font-bold">{title}</h2>
         <p className="mt-2">{message}</p>
         <div className="mt-4 flex justify-center gap-2">
           <button
-            className="w-1/2 bg-gray-300 px-4 py-2 rounded hover:bg-gray-400"
+            className="w-1/2 bg-gray-300 font-bold px-4 py-4 rounded hover:bg-gray-400"
             onClick={onCancel}
           >
             취소
           </button>
           <button
-            className="w-1/2 bg-cheese-bg text-white px-4 py-2 rounded hover:bg-red-600"
+            className="w-1/2 bg-cheese-bg font-bold px-4 py-4 rounded hover:bg-yellow-400"
             onClick={onConfirm}
           >
             확인
