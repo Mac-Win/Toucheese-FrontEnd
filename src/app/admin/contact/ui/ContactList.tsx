0df411fa05c6e3c4109901d447f8aa517b8e1e21
ruelline <ruelline91@naver.com>
2024-12-18 13:39:25 +0900

    ⚰️주석제거거

--- src/app/admin/contact/ui/ContactList.tsx
+++ src/app/admin/contact/ui/ContactList.tsx
@@ -23,12 +23,7 @@ function ContactList() {
             </tr>
           </thead>
           <tbody>
-            {/* {reservations.map((reservation, index) => ( */}
-            <tr
-            //   className={`${
-            //     index % 2 === 0 ? "bg-gray-100" : "bg-white"
-            //   } hover:bg-gray-50`}
-            >
+            <tr>
               <td className="py-4 px-2 border-r text-center">
                 {ContactDummy.customerNmae}
               </td>
@@ -52,62 +47,8 @@ function ContactList() {
                 {ContactDummy.status}
               </td>
             </tr>
-            {/* ))} */}
           </tbody>
         </table>
-
-        {/* <div className="md:hidden">
-          {reservations.map((reservation, index) => (
-            <div
-              key={index}
-              className={`mb-4 p-4 rounded-lg shadow ${
-                index % 2 === 0 ? "bg-gray-100" : "bg-white"
-              }`}
-            >
-              <p className="font-semibold">이름: {reservation.customerName}</p>
-              <p>전화번호: {reservation.customerPhone}</p>
-              <p>스튜디오 이름: {reservation.studioName}</p>
-              <p>예약 날짜: {reservation.createDate}</p>
-              <p>예약 시간: {reservation.createTime}</p>
-              <p>총 가격: {reservation.totalPrice}원</p>
-              <details>
-                <summary className="cursor-pointer font-semibold text-blue-600">
-                  상세 내용 보기
-                </summary>
-                <p>상품 이름: {reservation.productName}</p>
-                <p>상품 가격: {reservation.productPrice}원</p>
-                <p>선택 옵션:</p>
-                {reservation.selectAddOptions.map((selectAddOption) => (
-                  <p key={selectAddOption.selectOptionId} className="ml-4">
-                    - {selectAddOption.selectOptionName} (
-                    {selectAddOption.selectOptionPrice}원)
-                  </p>
-                ))}
-                <div className="mt-2">
-                  <label htmlFor={`status-${reservation.reservationId}`}>
-                    예약 상태:
-                  </label>
-                  <select
-                    name="status"
-                    id={`status-${reservation.reservationId}`}
-                    className="px-3 py-2 border rounded-md"
-                    defaultValue={reservation.status}
-                    onChange={(e) =>
-                      handleStatusChange(
-                        reservation.reservationId,
-                        e.target.value
-                      )
-                    }
-                  >
-                    <option value="예약접수">예약접수</option>
-                    <option value="예약확정">예약확정</option>
-                    <option value="예약취소">예약취소</option>
-                  </select>
-                </div>
-              </details>
-            </div>
-          ))}
-        </div> */}
       </div>
     </>
   );
