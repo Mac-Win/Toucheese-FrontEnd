fcf0a58b05d683e19c2eae5b5667b7e506450762
ruelline <ruelline91@naver.com>
2024-12-19 21:03:54 +0900

    🚚import 경로 수정

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import ReservationList from "./reservation/UI/ReservationList";
+import ReservationList from "./reservation/ui/ReservationList";
 import Pagination from "./components/pagination";
 import { useAdminReservation } from "./hooks/AdminReservation";
 
